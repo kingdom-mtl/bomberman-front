ecdcc9b845db0f494882443cb6ab74f973767dbb
Aurélien <aurelien.digout@adsdata.ca>
2019-06-10 23:22:23 -0400

    create server with socket.io

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import SocketIOClient from 'socket.io-client';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+    // Creating the socket-client instance will automatically connect to the server.
+  SocketIOClient('http://localhost:3000');
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
